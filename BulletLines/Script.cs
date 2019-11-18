810173688a663eae28276661c2647ce2e7414473
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-18 00:47:32 -0300

    Renamed BulletLinesV to BulletLines due to a Visual Studio mistake

--- BulletLines/Script.cs
+++ BulletLines/Script.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BulletLinesV
+namespace BulletLines
 {
-    public class Class1
+    public class BulletLines
     {
     }
 }
