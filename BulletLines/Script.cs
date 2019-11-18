3f3f8c6ee723627702a3a1545bbe4fdc1f79d430
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-18 00:49:04 -0300

    Added basic script

--- BulletLines/Script.cs
+++ BulletLines/Script.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GTA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace BulletLines
 {
-    public class BulletLines
+    public class BulletLines : Script
     {
+        public BulletLines()
+        {
+
+        }
     }
 }
