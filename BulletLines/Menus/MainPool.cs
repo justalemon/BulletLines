bcca08f1f87e2d1a65377985b016b0ea70df3fd4
Hannele Ruiz <justlemoncl@gmail.com>
2019-11-23 05:05:56 -0300

    Added custom menu pool

---
+++ BulletLines/Menus/MainPool.cs
@@ -0,0 +1,21 @@
+﻿using NativeUI;
+
+namespace BulletLines.Menus
+{
+    /// <summary>
+    /// Customized menu pool with all of the menus that we need.
+    /// </summary>
+    public class MainPool : MenuPool
+    {
+        /// <summary>
+        /// The menu with configuration values.
+        /// </summary>
+        public ConfigMenu Menu { get; } = new ConfigMenu();
+
+        public MainPool() : base()
+        {
+            // Add the menu to our pool
+            Add(Menu);
+        }
+    }
+}
